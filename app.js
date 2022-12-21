31006f98869a335e80bdd09dc5f1d617ff4d774c
sheine1234 <sheine.siro@gmail.com>
2022-12-20 23:25:42 -0300

    Subo proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/views/home.html");
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
+
+
